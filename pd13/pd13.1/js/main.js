794b06221563e0e520522d04edcd82378d1822f5
Makson4ik203 <makskuzia6@gmail.com>
2023-12-06 15:21:19 +0200

    PD12 solutions

--- pd13/pd13.1/js/main.js
+++ pd13/pd13.1/js/main.js
@@ -2,7 +2,7 @@ let x = 5;
 let y = 10;
 
 if (x > y) {
-  console.log("Didesnis skaičius: y");
-} else if (y > x) {
+  console.log("Didesnis skaičius: x");
+} else if (x < y) {
   console.log("Didesnis skaičius: y");
 }

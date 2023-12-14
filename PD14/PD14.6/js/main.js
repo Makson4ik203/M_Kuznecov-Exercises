bf0a7d9fd2e54733fffee48328938391114c408f
Makson4ik203 <makskuzia6@gmail.com>
2023-12-14 09:44:12 +0200

    PD14 solution

---
+++ PD14/PD14.6/js/main.js
@@ -0,0 +1,6 @@
+let eilute = 'JavaScript for all';
+
+// Sukuriame masyvą iš eilutės, apverčiame ir sujungiame atgal į eilutę
+let atvirksciaiEilute = Array.from(eilute).reverse().join('');
+
+console.log(atvirksciaiEilute);

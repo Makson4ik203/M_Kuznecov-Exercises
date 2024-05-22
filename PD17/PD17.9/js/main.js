c6882a91ebc8ebb5f8c2b90b7f7dc493530e5055
Makson4ik203 <makskuzia6@gmail.com>
2024-05-22 09:28:07 +0300

    PD17.9 solution

--- PD17/PD17.9/js/main.js
+++ PD17/PD17.9/js/main.js
@@ -1,11 +1,8 @@
-function logElementsByClass(className) {
-  const elements = document.getElementsByClassName(className);
-  console.log(`Elements with class '${className}':`);
-  for (let i = 0; i < elements.length; i++) {
-    console.log(elements[i]);
-  }
-}
-
-document.getElementById("findElementsButton").addEventListener("click", () => {
-  logElementsByClass("myClass");
-});
+document
+  .getElementById("findElementsButton")
+  .addEventListener("click", function () {
+    var elements = document.getElementsByClassName("myClass");
+    for (var i = 0; i < elements.length; i++) {
+      console.log(elements[i]);
+    }
+  });
